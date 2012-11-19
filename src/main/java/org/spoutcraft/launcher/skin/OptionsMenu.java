e80b6c76b440fc596345d3f590bd978014ab04de
Andrey Sinitsyn <asn007@nextgen.su>
2012-11-19 23:07:21 +0400

    Revert "Ok, trying to remove launcher update system"
    This reverts commit 28d3c0b2b872238959e4c3efaf49c2379c0ee7ee.

--- src/main/java/org/spoutcraft/launcher/skin/OptionsMenu.java
+++ src/main/java/org/spoutcraft/launcher/skin/OptionsMenu.java
@@ -242,7 +242,6 @@ public class OptionsMenu extends JDialog implements ActionListener {
 		return Channel.getType(selection);
 	}
 
-	@SuppressWarnings("restriction")
 	private void populateMemory(JComboBox memory) {
 		long maxMemory = 1024;
 		String architecture = System.getProperty("sun.arch.data.model", "32");
